7a0c788d11c4ad2c1947e46f06d36a99030d4129
Marcelo Borges <git@marceloborges.dev>
2025-02-22 18:15:53 +0000

    feat: update dependencies to last version

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,68 +8,58 @@ use embedded_graphics::{
 use embedded_hal_bus::spi::ExclusiveDevice;
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl,
     delay::Delay,
-    gpio::{Input, Io, Level, Output, NO_PIN},
-    peripherals::Peripherals,
-    prelude::*,
-    spi::{master::Spi, SpiMode},
-    system::SystemControl,
+    gpio::{Input, Level, Output},
+    main,
+    spi::{
+        master::{Config, Spi},
+        Mode,
+    },
+    time::RateExtU32,
 };
 use profont::PROFONT_24_POINT;
-use weact_studio_epd::graphics::{Display290TriColor, DisplayTriColor};
-use weact_studio_epd::{
-    graphics::{buffer_len, DisplayRotation},
-    TriColor, WeActStudio290TriColorDriver,
-};
+use weact_studio_epd::graphics::Display290TriColor;
+use weact_studio_epd::{graphics::DisplayRotation, TriColor, WeActStudio290TriColorDriver};
 
 // epaper connections:
 // DC: 21, RST: 22, BUSY: 23, CS/SS: 15, SCK: 6, MISO: -1, MOSI: 7
 
-#[entry]
+#[main]
 fn main() -> ! {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let mut delay = Delay::new(&clocks);
-
     esp_println::logger::init_logger_from_env();
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let delay = Delay::new();
 
-    log::info!("Intializing SPI Bus...");
+    log::info!("Initializing SPI Bus...");
 
-    let sclk = io.pins.gpio2;
-    let mosi = io.pins.gpio3;
-    let cs = io.pins.gpio7;
-    let dc = io.pins.gpio6;
-    let rst = io.pins.gpio11;
-    let busy = io.pins.gpio12;
+    let sclk = peripherals.GPIO2;
+    let mosi = peripherals.GPIO3;
+    let cs = peripherals.GPIO7;
+    let dc = peripherals.GPIO6;
+    let rst = peripherals.GPIO11;
+    let busy = peripherals.GPIO12;
 
-    let mut spi_bus = Spi::new(peripherals.SPI2, 100.kHz(), SpiMode::Mode0, &clocks).with_pins(
-        Some(sclk),
-        Some(mosi),
-        NO_PIN,
-        NO_PIN, // cs is handled by the exclusive device?
-    );
-
-    // Convert pins into InputPins and OutputPins
-    /*
-        CS: OutputPin,
-        BUSY: InputPin,
-        DC: OutputPin,
-        RST: OutputPin,
-    */
     let cs = Output::new(cs, Level::High);
     let busy = Input::new(busy, esp_hal::gpio::Pull::Up);
     let dc = Output::new(dc, Level::Low);
     let rst = Output::new(rst, Level::High);
 
-    log::info!("Intializing SPI Device...");
-    let spi_device = ExclusiveDevice::new(spi_bus, cs, delay).expect("SPI device initialize error");
+    let spi = Spi::new(
+        peripherals.SPI2,
+        Config::default()
+            .with_frequency(100.kHz())
+            .with_mode(Mode::_0),
+    )
+    .unwrap()
+    .with_sck(sclk)
+    .with_mosi(mosi);
+
+    log::info!("Initializing SPI Device...");
+    let spi_device = ExclusiveDevice::new(spi, cs, delay).expect("SPI device initialize error");
     let spi_interface = SPIInterface::new(spi_device, dc);
 
     // Setup EPD
-    log::info!("Intializing EPD...");
+    log::info!("Initializing EPD...");
     let mut driver = WeActStudio290TriColorDriver::new(spi_interface, busy, rst, delay);
     let mut display = Display290TriColor::new();
     display.set_rotation(DisplayRotation::Rotate90);
@@ -97,8 +87,10 @@ fn main() -> ! {
     // Update display
     driver.full_update(&display).unwrap();
 
+    let mut i = 0;
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        log::info!("Hello world! {i}");
+        delay.delay_millis(1000);
+        i += 1;
     }
 }
